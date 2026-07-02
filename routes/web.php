2fedeba1dfba065f1c73e738c37574468ccc0c2b
Smor <andkarido@gmail.com>
2026-07-02 10:09:45 +0200

    feat: Added basic crud for characters and testing

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CharacterController;
 use Illuminate\Support\Facades\Route;
 
 Route::inertia('/', 'welcome')->name('home');
@@ -7,5 +8,10 @@ Route::inertia('/', 'welcome')->name('home');
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::inertia('dashboard', 'dashboard')->name('dashboard');
 });
+
+Route::post('/characters', [CharacterController::class, 'store'])->middleware('auth');
+Route::get('/my-characters', [CharacterController::class, 'index'])->middleware('auth');
+Route::patch('/characters/{character}', [CharacterController::class, 'update'])->middleware('auth');
+Route::delete('/characters/{character}', [CharacterController::class, 'destroy'])->middleware('auth');
 
 require __DIR__.'/settings.php';
