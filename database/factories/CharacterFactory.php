778476a67307cf32da7abbcdd9aefcf77580479d
Smor <andkarido@gmail.com>
2026-07-02 13:58:29 +0200

    refactor: Updated controller, routes and factory for character

--- database/factories/CharacterFactory.php
+++ database/factories/CharacterFactory.php
@@ -19,6 +19,15 @@ class CharacterFactory extends Factory
     {
         return [
             'name' => fake()->name(),
+            'gender' => fake()->text(50),
+            'skin_color' => fake()->text(50),
+            'eye_color' => fake()->text(50),
+            'eye_shape' => fake()->text(50),
+            'hair_color' => fake()->text(50),
+            'nose_color' => fake()->text(50),
+            'mouth_color' => fake()->text(50),
+            'isShared' => fake()->boolean(),
+            'isAuthorized' => fake()->boolean(),
         ];
     }
 }
