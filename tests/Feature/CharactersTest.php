766939f637881968108e779a3afecbc7bfb11d9e
Smor <andkarido@gmail.com>
2026-07-02 11:43:10 +0200

    test: Updated character test

--- tests/Feature/CharactersTest.php
+++ tests/Feature/CharactersTest.php
@@ -4,6 +4,7 @@ use App\Models\Character;
 use App\Models\User;
 use Inertia\Testing\AssertableInertia;
 
+use function Pest\Laravel\actingAs;
 use function Pest\Laravel\assertDatabaseHas;
 use function Pest\Laravel\assertDatabaseMissing;
 use function Pest\Laravel\delete;
@@ -14,6 +15,22 @@ use function Pest\Laravel\post;
 beforeEach(function () {
     $this->user = User::factory()->create();
     $this->actingAs($this->user);
+});
+
+it('can render shared characters page', function () {
+    get('/characters')
+        ->assertStatus(200)
+        ->assertInertia(fn (AssertableInertia $page) => $page
+            ->component('Characters')
+        );
+});
+
+it('can render character creation form', function () {
+    get('/characters/create')
+        ->assertStatus(200)
+        ->assertInertia(fn (AssertableInertia $page) => $page
+            ->component('CharacterCreate')
+        );
 });
 
 it('can be created by user', function () {
@@ -27,6 +44,7 @@ it('can be created by user', function () {
     ]);
 });
 
+/*
 it('can be viewed by user', function () {
     $character = Character::factory()->for($this->user)->create(['name' => 'john']);
 
@@ -36,6 +54,29 @@ it('can be viewed by user', function () {
             component('MyCharacters')->
             has('characters')
         );
+});
+*/
+
+it('can render character detail page', function () {
+    $character = Character::factory()->for($this->user)->create(['name' => 'john']);
+
+    get("/characters/{$character->id}")
+        ->assertStatus(200)
+        ->assertInertia(fn (AssertableInertia $page) => $page
+            ->component('CharacterDetail')
+            ->has('character')
+        );
+});
+
+it('can render character edit form', function () {
+    $character = Character::factory()->for($this->user)->create(['name' => 'john']);
+
+    get("/characters/{$character->id}/edit")
+        ->assertStatus(200)
+        ->assertInertia(fn (AssertableInertia $page) => $page
+            ->component('CharacterEdit')
+        );
+
 });
 
 it('can be edited by user', function () {
@@ -52,6 +93,18 @@ it('can be edited by user', function () {
     ]);
 });
 
+it('can only be edited by user who created character', function () {
+    $character = Character::factory()->for($this->user)->create(['name' => 'john']);
+
+    $otherUser = User::factory()->create();
+
+    actingAs($otherUser)
+        ->patch("/characters/{$character->id}", [
+            'name' => 'mark',
+        ])
+        ->assertForbidden();
+});
+
 it('can be deleted by user', function () {
     $character = Character::factory()->for($this->user)->create(['name' => 'john']);
 
@@ -59,4 +112,14 @@ it('can be deleted by user', function () {
         ->assertRedirect();
 
     assertDatabaseMissing('characters', ['id' => $character->id]);
+});
+
+it('can only be deleted by user who created character', function () {
+    $character = Character::factory()->for($this->user)->create(['name' => 'john']);
+
+    $otherUser = User::factory()->create();
+
+    actingAs($otherUser)
+        ->delete("/characters/{$character->id}")
+        ->assertForbidden();
 });
