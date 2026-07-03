dda1273ab9b2b88c8f55c39d3bfb22cd301b7f1c
Smor <andkarido@gmail.com>
2026-07-03 16:42:18 +0200

    feat: Created footer component

---
+++ resources/js/components/app-footer.tsx
@@ -0,0 +1,7 @@
+export function AppFooter() {
+    return (
+        <footer className="flex h-16 items-center border-t border-sidebar-border/80 bg-[var(--main-dark-color)] px-4">
+            PixelVerse Studios Footer
+        </footer>
+    );
+}
