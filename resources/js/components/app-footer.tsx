df8816251b455663eee1965205cbd6776e03f72d
Smor <andkarido@gmail.com>
2026-07-04 23:03:11 +0200

    feat: Added character creation form page

--- resources/js/components/app-footer.tsx
+++ resources/js/components/app-footer.tsx
@@ -5,7 +5,7 @@ export function AppFooter() {
         <footer className="flex h-24 items-center justify-between border-t border-sidebar-border/80 bg-[var(--main-dark-color)] px-4">
             <h2>PixelVerse Studios</h2>
 
-            <div className="flex flex-col space-y-4">
+            <div className="flex flex-col space-y-4 text-right">
                 <Link href={'/terms'}>Terms and Service</Link>
                 <Link href={'/privacy'}>Privacy Policy</Link>
             </div>
