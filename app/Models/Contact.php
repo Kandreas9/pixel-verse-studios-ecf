9541cfc7d92377ad03085269c22631fe4c7d083f
Smor <andkarido@gmail.com>
2026-07-04 05:55:50 +0200

    feat: Setup mongodb connection

---
+++ app/Models/Contact.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use MongoDB\Laravel\Eloquent\Model;
+
+class Contact extends Model
+{
+    protected $connection = 'mongodb';
+
+    protected $collection = 'contacts';
+
+    protected $fillable = [
+        'email',
+        'name',
+        'details',
+    ];
+}
